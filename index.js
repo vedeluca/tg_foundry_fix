070c922f5b5cf24b07935703557cf8a4ce0e331e
vedeluca <vedeluca@comcast.net>
2024-11-24 21:05:05 -0500

    last third

--- index.js
+++ index.js
@@ -60,15 +60,22 @@ class TestChatMessage{
 	}
 }
 
-function mapGenerator(map, roll, ConMod, Prof){
+/**
+* @param {number} map
+* @param {Object} roll
+* @param {number} conMod
+* @param {number} prof
+* @returns {HTMLElement}
+*/
+function mapGenerator(map, roll, conMod, prof){
 	const mapDiv = document.createElement("div");
 	const mapSpanFront = document.createElement("span");
 	mapSpanFront.textContent = `MAP ${map}: `;
 	const mapStrong = document.createElement("strong");
-	mapStrong.textContent = roll.total + ConMod + Prof + map;
+	mapStrong.textContent = roll.total + conMod + prof + map;
 	const mapSpanBack = document.createElement("span");
 	const penalty = (map === 0) ? "" : ` ${map}`;
-	mapSpanBack.textContent = ` (${roll.total} + ${ConMod} + ${Prof}${penalty})`;
+	mapSpanBack.textContent = ` (${roll.total} + ${conMod} + ${prof}${penalty})`;
 	mapDiv.appendChild(mapSpanFront);
 	mapDiv.appendChild(mapStrong);
 	mapDiv.appendChild(mapSpanBack);
@@ -76,20 +83,22 @@ function mapGenerator(map, roll, ConMod, Prof){
 }
 
 async function crabAttackRoll(){
-	const ConMod = actor.abilities.con.mod;
-	const Prof = (actor.attributes.spellDC.value - 10 - ConMod);
+	const conMod = actor.abilities.con.mod;
+	const prof = (actor.attributes.spellDC.value - 10 - conMod);
 	const roll = (typeof Roll === "function") ? await new Roll("1d20").roll() : await new TestRoll("1d20").roll();
 	const attackContainer = document.createElement("div");
+	
 	const attackHeading = document.createElement("h2");
 	attackHeading.textContent = "Attack Roll";
 	attackContainer.appendChild(attackHeading);
 	const critHeading = document.createElement("h3");
+	
 	attackContainer.appendChild(critHeading);
-	attackContainer.appendChild(mapGenerator(0, roll, ConMod, Prof));
-	attackContainer.appendChild(mapGenerator(-5, roll, ConMod, Prof));
-	attackContainer.appendChild(mapGenerator(-10, roll, ConMod, Prof));
+	attackContainer.appendChild(mapGenerator(0, roll, conMod, prof));
+	attackContainer.appendChild(mapGenerator(-5, roll, conMod, prof));
+	attackContainer.appendChild(mapGenerator(-10, roll, conMod, prof));
+	
 	const strongList = attackContainer.getElementsByTagName("strong");
-
 	if (roll.total === 1) {
 		critHeading.style.color = "red";
 		critHeading.textContent = "Natural 1!";
@@ -110,36 +119,35 @@ async function crabAttackRoll(){
 	meleeTraitsPar.textContent = "Melee Traits: Agile, Backswing, Forceful, Reach, or Sweep";
 	const rangedTraitsPar = document.createElement("p");
 	rangedTraitsPar.textContent = "Ranged Traits: Range Increment 100 & Volley 30 ft, Range Increment 50 & Propulsive, Range Increment 20 & Thrown";
+	
 	attackContainer.appendChild(damageHeading);
 	attackContainer.appendChild(damageTypesPar);
 	attackContainer.appendChild(meleeTraitsPar);
 	attackContainer.appendChild(rangedTraitsPar);
 
-	let dieCount = 0;
-
-	if (actor.level < 5) {
-		dieCount = 1;
-	} else if (actor.level < 9) {
-		dieCount = 2;
-	} else if (actor.level < 13) {
-		dieCount = 3;
-	} else if (actor.level < 17) {
-		dieCount = 4;
-	} else {
-		dieCount = 5;
-	}
-
-	let damageRoll = (typeof Roll === "function") ? await new Roll(`${dieCount}d8`).roll() : await new TestRoll(`${dieCount}d8`).roll();
-	let damage_results_html = `<br>
-	<h4>1 Action</h4>
-	Melee:  ${damageRoll.total+actor.abilities.str.mod} (Crit: ${2*(damageRoll.total+actor.abilities.str.mod)})
-	<br>
-	Ranged: ${damageRoll.total} (Crit: ${2*(damageRoll.total)})
-	<h4>2 Actions</h4>
-	Melee:  ${damageRoll.total+actor.abilities.str.mod+actor.abilities.con.mod} (Crit: ${2*(damageRoll.total+actor.abilities.str.mod+actor.abilities.con.mod)})
-	<br>
-	Ranged: ${damageRoll.total+actor.abilities.con.mod} (Crit: ${2*(damageRoll.total+actor.abilities.con.mod)})`;
+	const dieCount = Math.ceil(actor.level / 4);
+	const damageRoll = (typeof Roll === "function") ? await new Roll(`${dieCount}d8`).roll() : await new TestRoll(`${dieCount}d8`).roll();
 	
+	const oneActionHeading = document.createElement("h4");
+	oneActionHeading.textContent = "1 Action";
+	const oneMeleePar = document.createElement("p");
+	oneMeleePar.textContent = `Melee:  ${damageRoll.total + actor.abilities.str.mod} (Crit: ${2 * (damageRoll.total + actor.abilities.str.mod)})`;
+	const oneRangedPar = document.createElement("p");
+	oneRangedPar.textContent = `Ranged: ${damageRoll.total} (Crit: ${2*(damageRoll.total)})`;
+	const twoActionHeading = document.createElement("h4");
+	twoActionHeading.textContent = "2 Action";
+	const twoMeleePar = document.createElement("p");
+	twoMeleePar.textContent = `Melee:  ${damageRoll.total + actor.abilities.str.mod + actor.abilities.con.mod} 
+		(Crit: ${2*(damageRoll.total + actor.abilities.str.mod + actor.abilities.con.mod)})`;
+	const twoRangedPar = document.createElement("p");
+	twoRangedPar.textContent = `Ranged: ${damageRoll.total+actor.abilities.con.mod} (Crit: ${2*(damageRoll.total + actor.abilities.con.mod)})`;
+	
+	attackContainer.appendChild(oneActionHeading);
+	attackContainer.appendChild(oneMeleePar);
+	attackContainer.appendChild(oneRangedPar);
+	attackContainer.appendChild(twoActionHeading);
+	attackContainer.appendChild(twoMeleePar);
+	attackContainer.appendChild(twoRangedPar);
 
 	if(typeof ChatMessage === "function"){
 		ChatMessage.create({
@@ -151,7 +159,7 @@ async function crabAttackRoll(){
 		TestChatMessage.create({
 			user: game.user._id,
 			speaker: TestChatMessage.getSpeaker({token: actor}),
-			content: attackContainer.innerHTML + damage_results_html
+			content: attackContainer.innerHTML
 		});
 	}
 }
